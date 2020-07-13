52a530018e19a3a9adb644e3875ee570d163ccf0
senseihimanshu <hsharma151197@gmail.com>
2020-07-14 01:47:33 +0530

    Partial work

--- models/evaluate.php
+++ models/evaluate.php
@@ -1,5 +1,7 @@
 <?php
 class EvaluateModel extends Model{
+    protected $userId;
+
     public function Index(){
         $this->query('SELECT * FROM table_question');
         $rows = $this->resultSet();
@@ -7,16 +9,36 @@ class EvaluateModel extends Model{
     }
 
     public function add(){
-        if(isset($_POST['submit'])){
-            $this->query('INSERT INTO table_user(EMAIL) VALUES (:email)');
-            $this->bind(':email', $_POST['email']);
-            $this->execute();
+        $id = $_GET['id'];
+        if($id < 1 || $id > 5){
+            $id = 1;
+        }
+        if($id == 1){
+            $this->query('SELECT * FROM table_question WHERE table_question.ID = '.$id);    
+        }
+        else{
+            $this->query('SELECT table_question.QUESTION, table_option.OPTION_ID, table_option.OPTION_TEXT FROM table_question INNER JOIN table_option ON table_question.ID = table_option.QUESTION_ID WHERE table_question.ID = '.$id);
+        }
+        $rows = $this->resultSet();
+        print($id);
+        print_r($rows);
 
-            echo 'Location: ' .ROOT_URL. 'evaluates';
-            header('Location: ' .ROOT_URL. 'evaluates');
+        if(isset($_POST['submit'])){
+            if($id == 1){
+                $this->query('INSERT INTO table_user(EMAIL) VALUES (:email)');
+                $this->bind(':email', $_POST['email']);
+                $this->execute();
+                if($this->lastInsertId()){
+                    $this->$userId = $this->lastInsertId();
+                    print($this->$userId);
+                    header('Location: ' .ROOT_URL. 'evaluates/add/' . $id+1);
+                }
+            }else{
+                $this->query('INSERT INTO table_user(EMAIL) VALUES (:email)');
+            }
         }
 
-        return;
+        return $rows;
     }
 
     public function get($id){
--- views/evaluates/add.php
+++ views/evaluates/add.php
@@ -1,24 +1,23 @@
 <div class="form-group">
     <form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
         <div class="form-group">
-            <label for="email">$viewmodel.QUESTION</label>
+            <label for="email"><?php echo $viewmodel.QUESTION ?></label>
             <input id="email" type="email" name="email" class="form-control" placeholder="Enter E-Mail" />
         </div>
 
         <input class="btn btn-primary" name="submit" type="submit" value="Submit" />
         <a class="btn btn-danger" href="/php.dev">Cancel</a>
         
-        <!-- <?php if($viewmodel.ID != 1) ?>
-            <?php foreach($viewmodel as $item) : ?>
-                <div class="well">
-                    <h3><?php echo $item['title']; ?></h3>
-                    <small><?php echo $item['create_date']; ?></small>
-                    <hr />
-                    <p><?php echo $item['body']; ?></p>
-                    <br />
-                    <a class="btn btn-default" href="<?php echo $item['link']; ?>" target="_blank">Go To Website</a>
-                </div>
-            <?php endforeach; ?>-->
+        <!-- <?php foreach($viewmodel as $item) : ?>
+            <div class="well">
+                <h3><?php echo $item['title']; ?></h3>
+                <small><?php echo $item['create_date']; ?></small>
+                <hr />
+                <p><?php echo $item['body']; ?></p>
+                <br />
+                <a class="btn btn-default" href="<?php echo $item['link']; ?>" target="_blank">Go To Website</a>
+            </div>
+        <?php endforeach; ?> -->
         
         
         <!-- <div class="form-check">
